b34ff5b53cf24bdeb93c5a4092993fff426f043d
klee0kai <klee0kai@gmail.com>
2021-05-14 21:42:24 +0300

    airflow test

--- MoexInfoDag.py
+++ MoexInfoDag.py
@@ -5,14 +5,18 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
-from extract.moex_info import extractMoexInfo
+# from extract.moex_info import extractMoexInfo
+
+
+def hi():
+    print("hi")
 
 with DAG('orders',
          schedule_interval=timedelta(days=2),
          ) as dag:
     extractMoexInfo = PythonOperator(
         task_id='moex_info',
-        python_callable=extractMoexInfo
+        python_callable=hi
     )
 
 
