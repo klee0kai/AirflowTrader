88e9bc7f0c6e6f581e9288fdafb14e660b3d0e73
klee0kai <klee0kai@gmail.com>
2021-05-15 15:42:37 +0300

    common info load

--- CommonInfoDag.py
+++ CommonInfoDag.py
@@ -1,3 +1,4 @@
+import logging
 import os, sys
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
@@ -6,16 +7,25 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.sensors.time_delta import TimeDeltaSensor
 from airflow.utils.dates import days_ago
 import configs
 
-from extract.moex_info import extractMoexInfo
+logging.basicConfig(level=logging.DEBUG)
+
+from extract.moex_info import extractMoexInfoAsync
 
 with DAG('Trader_Extract_Moex',
-         schedule_interval=timedelta(days=2),
+         schedule_interval=timedelta(hours=1),
          start_date=datetime.utcnow(),
          ) as dag:
+    weekSensor = TimeDeltaSensor(
+        delta=timedelta(days=7)
+    )
+
     extractMoexInfo = PythonOperator(
         task_id='moex_info',
-        python_callable=extractMoexInfo
+        python_callable=extractMoexInfoAsync
     )
+
+    weekSensor >> extractMoexInfo
--- extract/moex_info.py
+++ extract/moex_info.py
@@ -1,24 +1,69 @@
+import asyncio
+import logging
 import os, sys
 import shutil
 
 import configs
 import os.path
 
+import aiohttp
+import aiomoex
+import numpy as np
+import pandas as pd
+import configparser
+logging.basicConfig(level=logging.DEBUG)
+
 # extract common moex info:
 #     instruments
 #     engines
 #     markets
 
-MOEX_INFO_PATH = os.path.join(configs.AIRFLOW_DATA_PATH, "moex_info")
+COMMON_INFO_PATH = os.path.join(configs.AIRFLOW_DATA_PATH, "common/moex")
+MOEX_ISS_URL = "https://iss.moex.com"
+
+async def extractMoexInfoAsync():
+    request_url = f"{MOEX_ISS_URL}/iss.json"
+    async with aiohttp.ClientSession() as session:
+        iss = aiomoex.ISSClient(session, request_url)
+        data = await iss.get()
+        for key in data.keys():
+            df = pd.DataFrame(data[key])
+            with open(f"{COMMON_INFO_PATH}/{key}.csv", "w") as f:
+                f.write(df.to_csv())
+                print(f"created {f.name}")
+            with open(f"{COMMON_INFO_PATH}/{key}.txt", "w") as f:
+                f.write(df.to_string())
+                print(f"created {f.name}")
 
 
-def extractMoexInfo():
-    shutil.rmtree(MOEX_INFO_PATH, ignore_errors=True)
-    os.makedirs(MOEX_INFO_PATH, exist_ok=True)
+async def extractMoexSecuritiesAsync():
+    start = 0
+    async with aiohttp.ClientSession() as session:
+        f_csv = open(f"{COMMON_INFO_PATH}/securities.csv", "w")
+        f_txt = open(f"{COMMON_INFO_PATH}/securities.txt", "w")
 
-    print("extractMoexInfo  ")
+        while True:
+            request_url = f"{MOEX_ISS_URL}/iss/securities.json?start={start}&land=ru"
+            iss = aiomoex.ISSClient(session, request_url)
+            data = await iss.get()
+            df = pd.DataFrame(data['securities'])
+            if len(df) <= 0:
+                break
+            start += len(df)
+            f_csv.write(df.to_csv())
+            f_txt.write(df.to_string())
+        print(f"created {f_csv.name} and {f_txt.name}")
+
+
+def extractMoexAllCommonInfo():
+    # shutil.rmtree(COMMON_INFO_PATH, ignore_errors=True)
+    os.makedirs(COMMON_INFO_PATH, exist_ok=True)
+
+    print(f"extract moex info to {COMMON_INFO_PATH}")
+    asyncio.run(extractMoexInfoAsync())
+    asyncio.run(extractMoexSecuritiesAsync())
     pass
 
 
 if __name__ == "__main__":
-    extractMoexInfo()
+    extractMoexAllCommonInfo()
