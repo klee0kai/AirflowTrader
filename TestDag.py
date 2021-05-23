c5cdefafaefc951f443e9240e9d6daf517bbf9fa
klee0kai <klee0kai@gmail.com>
2021-05-23 22:48:44 +0300

    test subdags 4

--- TestDag.py
+++ TestDag.py
@@ -1,6 +1,5 @@
 import os, sys
 
-from airflow.operators.subdag import SubDagOperator
 from airflow.sensors.time_delta import TimeDeltaSensor
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
@@ -8,8 +7,11 @@ from datetime import timedelta, datetime
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
+from airflow.exceptions import AirflowSkipException
 
 args = {
     'owner': 'airflow',
@@ -17,37 +19,35 @@ args = {
 
 now = datetime.utcnow()
 
+
+def skip_every_2_minute():
+    if datetime.utcnow().minute % 2 == 0:
+        raise AirflowSkipException()
+
+
 with DAG(
         dag_id='Test_TimeSensorTestDag',
         default_args=args,
-        schedule_interval=timedelta(minutes=3),
-        start_date=now - timedelta(minutes=10),
+        schedule_interval=timedelta(minutes=1),
+        start_date=now - timedelta(minutes=1),
         max_active_runs=1
 ) as dag:
+    skipMinute_op = PythonOperator(
+        task_id='skip_minute',
+        python_callable=skip_every_2_minute
+    )
 
-    with DAG(
-            dag_id=dag.dag_id + ".commonDag",
-            default_args=args,
-            schedule_interval=timedelta(minutes=10),
-            start_date=now - timedelta(minutes=10),
-            max_active_runs=1
-    ) as subdag_common:
-        run_this_10_op = BashOperator(
-            task_id=subdag_common.dag_id + '-run_this',
-            bash_command="echo '10 minute tick'",
-        )
-
-    run_this_op = BashOperator(
+    run_this = BashOperator(
         task_id='run_this',
         bash_command="echo 'minute tick'",
     )
 
-    subdag_op = SubDagOperator(
-        task_id='commonDag',
-        subdag=subdag_common,
-    )
+    skipMinute_op >> run_this
 
-    subdag_op >> run_this_op
+    run_this_every_start = BashOperator(
+        task_id='run_this_tick',
+        bash_command="echo tick",
+    )
 
 if __name__ == "__main__":
     dag.cli()
