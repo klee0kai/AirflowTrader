f9901dbd74ce8aa93148c316c610fbc66a3f3a5b
klee0kai <klee0kai@gmail.com>
2021-05-23 18:25:38 +0300

    test time sensor

---
+++ TestDag.py
@@ -0,0 +1,41 @@
+import os, sys
+
+from airflow.sensors.time_delta import TimeDeltaSensor
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'airflow',
+}
+
+with DAG(
+        dag_id='TestDag',
+        default_args=args,
+        schedule_interval='0 0 * * *',
+        start_date=days_ago(2)
+) as dag:
+    minuteSensor = TimeDeltaSensor(
+        task_id='minute',
+        delta=timedelta(minutes=1)
+    )
+
+    run_this = BashOperator(
+        task_id='run_this',
+        bash_command="echo 'minute tick'",
+    )
+
+    minuteSensor >> run_this
+
+    run_this_every_start = BashOperator(
+        task_id='run_this',
+        bash_command="echo tick",
+    )
+
+if __name__ == "__main__":
+    dag.cli()
